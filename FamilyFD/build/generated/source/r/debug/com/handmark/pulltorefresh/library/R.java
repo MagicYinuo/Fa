45575997fe55ac709d83f9129311435033c78351
Jack <yangjie-201@163.com>
2016-08-15 15:48:02 +0800

    增加guide引导页面按钮

--- FamilyFD/build/generated/source/r/debug/com/handmark/pulltorefresh/library/R.java
+++ FamilyFD/build/generated/source/r/debug/com/handmark/pulltorefresh/library/R.java
@@ -44,9 +44,9 @@ public final class R {
 	public static final class drawable {
 		public static final int default_ptr_flip = 0x7f02009e;
 		public static final int default_ptr_rotate = 0x7f02009f;
-		public static final int indicator_arrow = 0x7f0200dc;
-		public static final int indicator_bg_bottom = 0x7f0200dd;
-		public static final int indicator_bg_top = 0x7f0200de;
+		public static final int indicator_arrow = 0x7f0200dd;
+		public static final int indicator_bg_bottom = 0x7f0200de;
+		public static final int indicator_bg_top = 0x7f0200df;
 	}
 	public static final class id {
 		public static final int both = 0x7f0c000a;
